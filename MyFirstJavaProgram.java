912fc71d818cf0dc7e76f196eaf98d37d4f65479
Inbaraj1888 <54296499+Inbaraj1888@users.noreply.github.com>
2019-08-20 13:08:55 +0900

    Update MyFirstJavaProgram.java

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -5,6 +5,6 @@ public class MyFirstJavaProgram {
     */
 
    public static void main(String []args) {
-      System.out.println("Hello World!!4"); // prints Hello World
+      System.out.println("Hello World!!5"); // prints Hello World
    }
 }
